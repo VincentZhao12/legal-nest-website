81059167efce6d7dfa6c2484badf034b000db122
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2021-06-26 17:13:12 -0700

    Added boilerplate things for react project

---
+++ src/firebase.ts
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/functions';
+import 'firebase/messaging';
+
+interface FirebaseConfig {
+    apiKey: string | undefined;
+    authDomain: string | undefined;
+    projectId: string | undefined;
+    storageBucket: string | undefined;
+    messagingSenderId: string | undefined;
+    appId: string | undefined;
+}
+
+const config: FirebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+export const db: firebase.firestore.Firestore = firebase.firestore();
+export const auth: firebase.auth.Auth = firebase.auth();
+export const storage: firebase.storage.Storage = firebase.storage();
